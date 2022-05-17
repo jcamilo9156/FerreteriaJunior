e51028555f42ccbacca4af5670ed88ecd9aa4507
jcamilo9156 <jcamilo9156@gmail.com>
2022-05-16 23:48:55 -0500

    .

--- src/php/cambio-password.php
+++ src/php/cambio-password.php
@@ -1,4 +1,5 @@
 <?php
+$user = $_POST ['userid'];
 $password1 = $_POST['password1'];
 $password2 = $_POST['password2'];
 
@@ -11,16 +12,16 @@ if ($password1 <> $password2) {
 
 if (preg_match('`^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{10}$`',$password1)){
     include('conexion.php');
-    $consulta = ("UPDATE usuarios" SET("") )
-    $resultado = mysqli_query($conexion, $consulta)); 
+    $consulta = "UPDATE usuarios  SET Clave = '$password1'";
+    $resultado = mysqli_query($conexion, $consulta); 
     echo "exito <br>";
+    echo "la nueva contraseña es $password1";
     include('cerrar-conexion.php');
-    
 
  } else {
      echo "Contraseña no cumple con la política";
  }
-
+// pendiente validación de contraseña actual
 
 
 ?>
