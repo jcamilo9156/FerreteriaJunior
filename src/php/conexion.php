e74fec1f81a28e24a83e399dfc84577135403793
jcamilo9156 <jcamilo9156@gmail.com>
2022-07-18 19:51:56 -0500

    .

--- src/php/conexion.php
+++ src/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR);
-$conexion = @mysqli_connect('localhost' , 'root' , '', 'bd_ferreteriajunior');
+$conexion = @mysqli_connect('sqlferreteriajunior.mysql.database.azure.com' , 'Juan@sqlferreteriajunior' , 'Sena2022*', 'bd_ferreteriajunior');
 if(!$conexion) {
     echo ("Error al conectar a la base de datos");
 }
