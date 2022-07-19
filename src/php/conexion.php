318afbdbd2d903bdc673f324870ad5a5f3346da6
jcamilo9156 <jcamilo9156@gmail.com>
2022-07-18 20:50:40 -0500

    .

--- src/php/conexion.php
+++ src/php/conexion.php
@@ -1,8 +1,15 @@
 <?php
-$con=mysqli_init(); 
-mysqli_real_connect($con, "sqlferreteriajunior.mysql.database.azure.com", "Juan@sqlferreteriajunior", "Sena2022*", "bd_ferreteriajunior", 3306);
-?>
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:serverferreteria.database.windows.net,1433; Database = bd_ferreteriajunior", "juan", "Sena2022*");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
+?>
 <?php
 /*
 mysqli_report(MYSQLI_REPORT_ERROR);
